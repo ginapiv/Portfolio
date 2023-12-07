8a683376065ad414b53e1b391c16b87bd0fbe6f1
ginapiv <144375635+ginapiv@users.noreply.github.com>
2023-12-07 15:53:24 -0500

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,3 +1,28 @@
-window.addEventListener("scroll", function () {
-    console.log("height: "+ window.innerHeight + "\n" +"width: "+window.innerWidth);
-}); 
+function postToGoogle() {
+    var field1 = $("#Name").val();
+    var field2 = $("#Email").val();
+    var field3 = $("#Phone").val();
+    var field4 = $("#Message").val();
+
+    $.ajax({
+      url: "{https://docs.google.com/forms/u/1/d/e/1FAIpQLSfucdxjGkujDPmL0vpGrJ16zjrayNkLGJg-8_7WBAVxwpwcdw/formResponse}",
+        
+      //google forum    
+      data: {
+        "entry.111334685": field1,
+        "entry.1680758593": field2,
+        "entry.1184312553": field3,
+        "entry.1804124301": field4,
+        "entry.192861214": field5
+      },
+      type: "POST",
+      dataType: "xml",
+      success: function (d) {
+        $('#contact').trigger('reset');
+      },
+      error: function (x, y, z) {
+        $('#contact').trigger('reset');
+      }
+    });
+    return false;
+  }
